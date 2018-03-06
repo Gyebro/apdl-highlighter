0343f1df7c1503410bbf1bfd7c0f65f8cb37c659
Tamás Pölöskei <poloskei@mm.bme.hu>
2018-03-06 10:33:45 +0100

    NL1, SOL1, ST3, H2 + hints

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-string filenames[] = {"R1", "R2", "H1", "H3", "R3", "S3", "SOL1", "ST1", "ST4", "P1"};
+string filenames[] = {"R1", "R2", "H1", "H3", "R3", "S3", "SOL1", "ST1", "ST4", "P1", "NL1", "ST3", "H2"};
 
 int main() {
     cout << "Running APDL highlighter\n";
-    highlighter h("config/rules.txt", "config/hints.txt", "config/config.txt");
+    highlighter h("config/rules.txt", "config/hints_eng.txt", "config/config.txt");
     for (string filename : filenames) {
         h.highlight("input/"+filename+".txt");
     }
